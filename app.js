49b69ce1dbdcf17bd3cb26741876cf32a32e8adf
RandySincoularUW <sincoular@wisc.edu>
2023-03-30 17:22:38 +0000

    updates

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+// Import the express module
 var express = require('express');
+
+// Initialize express by instantiating it and assign a variable 
+// called 'app' to it
 var app = express();
 
 // Root Route
 app.get('/', function (req, res) {
   console.log("in app.js.  The root route has been hit ...")
+  res.status(200)
   res.send('Hello World! from my new Geog 576 app');
 });
 
-app.listen(8000, function () {
+app.listen(8000, function (err) {
+  if (err) console.log("Error starting server.  Msg: " + err)
   console.log('Node.js and Express app listening on port 8000!');
 });
